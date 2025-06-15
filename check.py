cf5055816562488948f36610ae92f318b64cba4f
Vanshdalal314 <dalalvvvvansh@gmail.com>
2025-06-15 16:43:07 +0530

    completed

---
+++ check.py
@@ -0,0 +1,6 @@
+import traceback
+
+try:
+    from urllib3.packages.six.moves import urllib
+except Exception:
+    traceback.print_exc()
---
+++ src/helper.py
@@ -0,0 +1,36 @@
+import os
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
+from langchain.vectorstores import FAISS
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+
+load_dotenv()
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+os.environ['GOOGLE_API_KEY'] = GOOGLE_API_KEY
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text+= page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_vector_store(text_chunks):
+    embeddings = GoogleGenerativeAIEmbeddings()
+    vector_store = FAISS.from_texts(text_chunks, embeddings)
+    return vector_store
+
+def get_conversational_chain(vector_store):
+    llm=ChatGoogleGenerativeAI()
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    conversation_chain= ConversationalRetrievalChain.from_llm(llm=llm, retriever=vector_store.as_retriever(), memory=memory)
+    return conversation_chain
