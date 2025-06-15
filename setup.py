910d36969b1c83d4a3af9c540405b50e91dd210c
Vanshdalal314 <dalalvvvvansh@gmail.com>
2025-06-15 14:58:01 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'SEEKR',
+    version = '0.0.0',
+    author= 'Vansh',
+    author_email= 'dalalvanshb@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
